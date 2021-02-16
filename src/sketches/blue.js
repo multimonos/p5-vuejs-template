45a6e095caac758f9533d688f3ab4493b13e274d
Craig Hopgood <searaig@gmail.com>
2021-02-16 10:41:19 -0500

    update sample sketch

--- src/sketches/blue.js
+++ src/sketches/blue.js
@@ -9,15 +9,17 @@ export default p => {
 
     p.setup = () => {
         p.createCanvas(p.windowWidth, p.windowHeight);
-        ctr = new p5.Vector(p.width, p.height).mult(0.5)
         console.log('blue loaded')
     }
 
     p.draw = () => {
+        ctr = getCenter()
         p.fill(p.color(0, 0, 255, 8))
         p.rect(0, 0, p.width, p.height)
         p.fill(0, 0, 255)
         p.circle(p.frameCount, ctr.y, 100)
     }
-
+    const getCenter = () => {
+        return new p5.Vector(p.width, p.height).mult(0.5)
+    }
 }
